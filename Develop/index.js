fdea4fd23637c5f29f067ab575c599a96e383dbe
LawrenceSB24 <lawrs2022@gmail.com>
2022-02-26 17:05:10 -0500

    Added license badge

--- Develop/index.js
+++ Develop/index.js
@@ -51,21 +51,22 @@ inquire
             name: 'license',
             choices: [
                 'None',
-                'Apache-license-2.0',
-                'GNU-General-Public-License-v3.0',
-                'MIT-License',
-                'BSD-2--Clause-"Simplified"-License',
-                'BSD-3--Clause-"New"-or-"Revised"-License',
-                'Boost-Software-License-1.0',
-                'Creative-Commons-Zero-v1.0-Universal',
-                'Eclipse-Public-License',
-                'GNU-Affero-General-Public-License v3.0',
-                'GNU-General-Public-License-v2.0',
-                'GNU-Lesser-General-Public-License-v2.1',
-                'Mozilla-Public-License-2.0',
-                'The-Unlicensed'
+                'Apache license 2.0',
+                'GNU General Public License v3.0',
+                'MIT License',
+                'BSD 2-Clause "Simplified" License',
+                'BSD 3-Clause "New" or "Revised" License',
+                'Boost Software License 1.0',
+                'Creative Commons Zero v1.0 Universal',
+                'Eclipse Public License',
+                'GNU Affero General Public License v3.0',
+                'GNU General Public License v2.0',
+                'GNU Lesser General Public License v2.1',
+                'Mozilla Public License 2.0',
+                'The Unlicensed'
             ]
         },
+        // Contact information for Questions section
         {
             type: 'input',
             message: 'What is your email?',
@@ -75,7 +76,10 @@ inquire
             type: 'input',
             message: 'What is your GitHub username(?): ',
             name: 'Github'
-        }
+        },
+        // {
+        //     type: 'input'
+        // }
     ])
 // Conditions for README file to be written
 .then((answers) => {
@@ -90,7 +94,7 @@ const generateREADME = ({title, description, installation, usage, contribute, te
 `
 # ${title}
 
-[![License: ${license}](https://img.shields.io/badge/License-${license}.svg)]
+![GitHub](https://img.shields.io/github/license/${Github})
 
 ## Description
 
@@ -125,3 +129,19 @@ If you have any questions about the application, please contact me at:
 ## License
 This application and its contents are licensed under the ${license}
 `
+
+// List of badges for present license
+// Apache: [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+// GNU GPL v3: [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+// MIT: [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+// BSD 2: [![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)
+// BSD 3: [![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+// Boost: [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
+// Creative: [![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)
+// Eclipse: [![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
+// GNU Affero: [![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)
+// GNU GPL v2: [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+// GNU LGPL: [![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)
+// Mozilla: [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+// Unlicensed: [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+ 
